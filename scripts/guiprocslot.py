19c8891e19647f5fc9af5164900c4ca3531b2dd4
Po Cheng <po.cheng@technexion.com>
2020-05-26 18:04:23 +0800

    guiprocslot: fix message from BOOT MODE to eMMC BOOT MODE
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- scripts/guiprocslot.py
+++ scripts/guiprocslot.py
@@ -2862,14 +2862,14 @@ class QMessageDialog(QtGui.QDialog):
         elif msgtype == 'Restore':
             self.setIcon(self.style().standardIcon(getattr(QtGui.QStyle, 'SP_MessageBoxInformation')))
             self.setTitle("Restore Complete")
-            self.setContent('Please reset your board.\n(For boards with a boot jumper, set it to BOOT MODE.)')
+            self.setContent('Please power off and on your board.\n(For boards with a boot jumper, set it to eMMC BOOT MODE first.)')
         elif msgtype == 'Complete':
             self.setIcon(self.style().standardIcon(getattr(QtGui.QStyle, 'SP_MessageBoxInformation')))
             self.setTitle("Program Complete")
             # movie = QtGui.QMovie(':/res/images/error_edm-fairy_reset.gif')
             # movie.setScaledSize(QtCore.QSize(self.rect().width() / 2, self.rect().height() / 2))
             # self.setContent(movie)
-            self.setContent('Please reset your board.\n(For boards with a boot jumper, set it to BOOT MODE.)')
+            self.setContent('Please power off and on your board.\n(For boards with a boot jumper, set it to eMMC BOOT MODE first.)')
         elif msgtype == 'Interrupt':
             self.setIcon(self.style().standardIcon(getattr(QtGui.QStyle, 'SP_MessageBoxQuestion')))
             self.setTitle("Flashing images.")
