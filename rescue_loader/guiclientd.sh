13730f6778e3f753eb0ef22eadcd12ab11eeb075
Po Cheng <po.cheng@technexion.com>
2020-09-25 18:30:30 +0800

    guiclient.sh: fix checking width and height from fbset geometry
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- rescue_loader/guiclientd.sh
+++ rescue_loader/guiclientd.sh
@@ -4,12 +4,15 @@ detect_touch () {
 for i in /sys/class/input/input?
 do
   if ( grep -qE "EP000|EP079|EP082|EP085|EP0510|EP0512|EXC3146|EXC3160|P80H100" ${i}/name ); then
+    export QWS_MOUSE_PROTO="linuxinput:/dev/input/event1"
     export QWS="-touch -qws"
     return
   elif ( grep -qE "P80H60" ${i}/name ); then
+    export QWS_MOUSE_PROTO="linuxinput:/dev/input/event1"
     export QWS="-touch -qws"
     return
   elif ( grep -qE "ADS7846" ${i}/name ); then
+    export QWS_MOUSE_PROTO="linuxinput:/dev/input/event1"
     export QWS="-touch -qws"
     return
   else
@@ -18,11 +21,11 @@ do
 done
 }
 
-
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/var/run/dbus/session_bus_socket"
-if fbset | grep geometry | awk '{print (( test $2 < $3 ))}'; then
+W=$(fbset | grep geometry | awk '{print $2}')
+H=$(fbset | grep geometry | awk '{print $3}')
+if (( $W < $H )); then
   export QWS_DISPLAY="transformed:mmWidth140:mmHeight68:rot270 directfb:0"
-  export QWS_MOUSE_PROTO="linuxinput:/dev/input/event1"
 else
   export QWS_DISPLAY="directfb:0"
 fi
