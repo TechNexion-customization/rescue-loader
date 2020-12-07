2d5729bcdfa0b27af70bc3dbd0fcdb35c25adefb
Po Cheng <po.cheng@technexion.com>
2020-12-07 10:39:01 +0800

    guiclient.sh: Tune QWS_DISPLAY variable with panel dimension for font adjustment
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- rescue_loader/guiclientd.sh
+++ rescue_loader/guiclientd.sh
@@ -3,82 +3,117 @@
 detect_touch () {
 for i in /sys/class/input/input?
 do
-  if ( grep -qEi "powerkey" ${i}/name ); then
-    continue
-  elif ( grep -qE "EP000|EP079|EP082|EP085|EP0510|EP0512" ${i}/name ) && ( grep -qE "18" ${i}/id/bustype ); then
+  inputname=$(cat ${i}/name)
+  case "$inputname" in
+  EP000*|EP079*|EP082*|EP085*|EP0510*|EP0512*)
     eventid=$(ls ${i} | grep event[0-9])
     export QWS_MOUSE_PROTO="linuxinput:/dev/input/$eventid"
     export QWS="-touch -qws"
     return
-  elif ( grep -qE "EXC3146|EXC3160|P80H60|P80H100" ${i}/name ) && ( grep -qE "3" ${i}/id/bustype ); then
+    ;;
+  *EXC3146*|*EXC3160*|*P80H60*|*P80H100*)
     eventid=$(ls ${i} | grep event[0-9])
     export QWS_MOUSE_PROTO="linuxinput:/dev/input/$eventid"
     export QWS="-touch -qws"
     return
-  elif ( grep -qEi "USB.*MOUSE" ${i}/name ) && ( grep -qE "3" ${i}/id/bustype ); then
+    ;;
+  *Usb*Mouse|*USB*Mouse)
     eventid=$(ls ${i} | grep event[0-9])
     export QWS_MOUSE_PROTO="linuxinput:/dev/input/$eventid"
     export QWS="-qws"
-  else
+    ;;
+  *)
     export QWS="-qws"
-  fi
+    ;;
+  esac
 done
-
 }
 
 detect_screen_size () {
+  fbcount=$(ls -l /sys/class/graphics/fb? | wc -l)
   # filter out overlays and figure out the virtual_size for each fb?
+  FBS=()
   W=99999
   H=99999
-  FB=0
-  FBS=()
-  count=$(( $1-1 ))
-  while [ $count -ge 0 ]
+  count=0
+  while [ $count -lt $fbcount ]
   do
-    if ! grep -q "overlay" "/sys/class/graphics/fb${count}/fsl_disp_dev_property"; then
+    # workout smallest resolution, the loop should get the first device of the
+    # same resolution, therefore no need to check "overlay" in "fb?/fsl_disp_dev_property"
+    if [ -f "/sys/class/graphics/fb${count}/virtual_size" ]; then
       dW=$(cat "/sys/class/graphics/fb${count}/virtual_size" | cut -d"," -f1)
       dH=$(cat "/sys/class/graphics/fb${count}/virtual_size" | cut -d"," -f2)
       if [ $dW -lt $W -a $dH -lt $H ]; then
         W=$dW
         H=$dH
-        FB=$count
+        fbmain=$count
+        FBS+=( $count )
       fi
-      FBS+=( $count )
     fi
-    count=$(( $count-1 ))
+    count=$(( $count + 1 ))
   done
 }
 
-detect_display () {
-  fbcount=$(ls -l /sys/class/graphics/fb? | wc -l)
-  if [ "$fbcount" = "1" ]; then
-    fbsetting="LinuxFb:/dev/fb0"
+detect_multi () {
+  # e.g. QWS_DISPLAY="Multi: LinuxFb:/dev/fb0 LinuxFb:/dev/fb2"
+  if [ ${#FBS[@]} -gt 1 ]; then
+    fbsetting="Multi: "
   else
-    detect_screen_size $fbcount
-    # e.g. QWS_DISPLAY="Multi: LinuxFb:/dev/fb0 LinuxFB:/dev/fb2"
-    fbsetting="Multi: LinuxFb:/dev/fb${FB}"
-    if [ ! $W -eq 99999 -a ! $H -eq 99999 ]; then
-      for i in ${FBS[@]}
-      do
-        if [ ! $i -eq $FB ]; then
-          fbsetting="$fbsetting LinuxFb:/dev/fb${i}"
-        fi
-      done
-    fi
+    fbsetting=""
   fi
 }
 
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/var/run/dbus/session_bus_socket"
-detect_display
-W=$(fbset | grep geometry | awk '{print $2}')
-H=$(fbset | grep geometry | awk '{print $3}')
-if (( $W < $H )); then
-  export QWS_DISPLAY="Transformed:rot270:$fbsetting"
-else
-  export QWS_DISPLAY="$fbsetting"
-fi
+detect_transform () {
+  # e.g. QWS_DISPLAY="Transformed:rot270:LinuxFb:mmWidth140:mmHeight68:/dev/fb0"
+  dimension=""
+  for fb in ${FBS[@]}
+  do
+    W=$(cat "/sys/class/graphics/fb${fb}/virtual_size" | cut -d"," -f1)
+    H=$(cat "/sys/class/graphics/fb${fb}/virtual_size" | cut -d"," -f2)
+    if [ $W -lt $H ]; then
+      fbsetting="${fbsetting}Transformed:rot270:"
+      if grep -qE "EP000|EP079|EP082|EP085|EP0510|EP0512" <<< $inputname; then
+        dimension="mmWidth140:mmHeight68:"
+        portraight=${fb}
+      fi
+      return
+    fi
+  done
+}
 
+display_setting () {
+  if [ -n $fbmain ]; then
+    if [ "$fbmain" = "$portraight" ]; then
+        fbsetting="${fbsetting}LinuxFb:${dimension}/dev/fb${fbmain} "
+    else
+        fbsetting="${fbsetting}LinuxFb:/dev/fb${fbmain} "
+    fi
+  fi
+  for fb in ${FBS[@]}
+  do
+    if [ "$fb" = "$fbmain" ]; then
+      continue
+    fi
+    if [ "$fb" = "$portraight" ]; then
+        fbsetting="${fbsetting}LinuxFb:${dimension}/dev/fb${fb} "
+    else
+        fbsetting="${fbsetting}LinuxFb:/dev/fb${fb} "
+    fi
+  done
+}
+
+#
+# entry point
+#
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/var/run/dbus/session_bus_socket"
 detect_touch
+detect_screen_size
+detect_multi
+detect_transform
+display_setting
+
+qwsdisp=$(echo "${fbsetting}" | xargs)
+export QWS_DISPLAY="$qwsdisp"
 
 echo "QWS_DISPLAY: $QWS_DISPLAY"
 echo "QWS_MOUSE_PROTO: $QWS_MOUSE_PROTO"
